e446c5987483f5d035bf8de7d45bb91833e4c169
wojtekw92 <wwozniak92@gmail.com>
2018-10-27 16:49:21 +0200

    Kiil broadcast

--- server/rooms/defaultRoom.js
+++ server/rooms/defaultRoom.js
@@ -22,17 +22,26 @@ class DefaultRoom extends Room {
   // When a client leaves the room
   onLeave (client) {
     delete this.state.players[ client.sessionId ];
+    this.broadcast({
+      type: 'left',
+      data: {
+        id: client.sessionId
+      }
+    })
   }
 
   // When a client sends a message
   onMessage (client, data) {
      if ( data.type === 'position') {
       this.state.players[ client.sessionId ] = data.data;
-     }
-     else if (data.type === 'shoot') {
+     } else if (data.type === 'shoot') {
        data.shooter = client.sessionId;
        this.broadcast(data);
-     }
+     } else if (data.type === 'killed') {
+      data.data.killerId = client.sessionId;
+      this.broadcast(data);
+    }
+
   }
 
 
